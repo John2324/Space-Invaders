e67ec9e8e5471839cda5db15b23c73a8bdf22f30
John2324 <54280648+John2324@users.noreply.github.com>
2019-10-15 13:55:29 -0700

    Add files via upload

---
+++ js/screens/play.js
@@ -0,0 +1,14 @@
+game.PlayScreen = me.ScreenObject.extend({
+    /**
+     * action to perform on state change
+     */
+    onResetEvent : function () {
+        me.game.world.addChild(me.pool.pull("player"));
+    },
+  
+    /**
+     * action to perform when leaving this screen (state change)
+     */
+    onDestroyEvent : function () {
+    }
+  });
