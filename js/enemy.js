b5529a4ebaa958e3837728cc67d7c1606e6cfe1d
John <jmonte80@calstatela.edu>
2019-10-15 23:59:51 -0700

    Add collision function to Enemy Object

--- js/enemy.js
+++ js/enemy.js
@@ -6,6 +6,8 @@ game.Enemy = me.Entity.extend({
             height: 32
         }]);
         this.chooseShipImage();
+        this.body.setVelocity(0, 0);
+        this.body.collisionType = me.collision.types.ENEMY_OBJECT;
     },
 
     chooseShipImage: function () {
@@ -13,4 +15,12 @@ game.Enemy = me.Entity.extend({
         this.renderable.addAnimation("idle", [frame], 1);
         this.renderable.setCurrentAnimation("idle");
     },
+
+    update : function (time) {
+        this._super(me.Entity, "update", [time]);
+    
+        this.body.update();
+    
+        return true;
+    }
 });
